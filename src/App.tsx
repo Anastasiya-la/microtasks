d7c53b28f5706369d0d3edde6515de48e1036bac
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-07-12 23:36:33 +0300

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Header} from "./site/Header";
 import {Body} from "./site/Body";
 import {Footer} from "./site/Footer";
 import {NewComponent} from "./NewComponent";
+import {Button} from "./components/Button";
 
 function App() {
     const students = [
@@ -21,6 +22,18 @@ function App() {
         {id: 11, name: "Christopher", age: 100},
     ]
 
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber, age, address)
+    }
+
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
+    }
+
+    const Button3Foo = () => {
+        console.log('Iam stupid button')
+    }
+
     return (
         <div>
             <Header title={'NEW BODY'}/>
@@ -28,6 +41,10 @@ function App() {
             <Footer titleForFooter={'NEW FOOTER'}/>
             <hr/>
             <NewComponent students={students}/>
+            <hr/>
+            <Button name={'MyYouTubeChannel-1'} callBack={() => Button1Foo('Iam Vasya', 21, 'live in Minsk')}/>
+            <Button name={'MyYouTubeChannel-2'} callBack={() => Button2Foo('Iam Ivan')}/>
+            <Button name={'Stupid Button'} callBack={Button3Foo}/>
         </div>
     );
 }
